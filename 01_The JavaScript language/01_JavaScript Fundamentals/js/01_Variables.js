efbc4d99dd3323f5a855deacc276251e827cdcc6
Ashutosh Naik <naikashu2401@gmail.com>
2023-10-14 20:23:28 +0530

    Variables

--- 01_The JavaScript language/01_JavaScript Fundamentals/js/01_Variables.js
+++ 01_The JavaScript language/01_JavaScript Fundamentals/js/01_Variables.js
@@ -1,1 +1,33 @@
 "use strict";
+
+let message = "Hello"; // store the string 'Hello' in the variable named message
+console.log(message); // shows the variable content
+
+let user = "John";
+let age = 25;
+let greeting = "Hello";
+
+let hello1 = "Hello World";
+let hello2;
+// copy 'Hello world' from hello1 into hello2
+hello2 = hello1;
+// now two variables hold the same data
+console.log(hello1);
+console.log(hello2);
+
+const dateOfBirth = "02.04.2001";
+console.log(dateOfBirth);
+
+const COLOR_RED = "#FF0000";
+const COLOR_GREEN = "#00FF00";
+const COLOR_BLUE = "#0000FF";
+const COLOR_ORANGE = "#FF7F00";
+let color = COLOR_ORANGE;
+console.log(color);
+
+let admin;
+let name;
+name = "John";
+admin = name;
+console.log(admin);
+console.log(name);
